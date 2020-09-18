aa73967ebdd80dbb494941955f109c9510630cf9
Ármin A. Rába <47749254+ArminARaba@users.noreply.github.com>
2020-09-18 17:23:22 +0200

    P1 - elore

--- Px_programok/src/main.cpp
+++ Px_programok/src/main.cpp
@@ -1,35 +1,68 @@
-//Változók: motorok statikusak, a pot változó, a szervók globális változók
-void P1()//Előre menet
+/*Létrehozott változók
+* char data[10]; //Ebben(a bufferben) tároljuk a protokol szerinti értékeket - GLOBÁLIS
+* char* bgn = "#"; //Ezzel Kezdődik - GLOBÁLIS
+* char* motB = "1"; //Baldldali motor - LOKÁLIS
+* char* motJ = "0"; //Jobboldali motor - LOKÁLIS
+* char* nd = "*"; //Ezzel végződik - GLOBÁLIS
+* A 4. szervónak x_Servo nevet adtam, hogy tudjak haladni a programmal
+*/
+
+void P1() //Előre menet
 {
-  //Serial.println("Elore");
-  while (scanner() != 0)
+  char *motB = ""; //Baloldali motor
+  char *motJ = ""; //Jobboldali motor
+  while (scanner() != 0) //Amíg az érték nem egyenlő a nullával, csinálja az alábbit:
   {
     int btA = digitalRead(BA);
     int btB = digitalRead(BB);
-    if (btA == 0)
+    if (btA == 0) //Ha a baloldali piros gomb le van nyomva, menjen balra
     {
       int potV = analogRead(POT);
-      potV = map(potV,POTMETER_MIN,POTMETER_MAX,0,6);
-      Serial.println("BALRA ELORE"); 
+      potV = map(potV, POTMETER_MIN, POTMETER_MAX, 0, 6);
+      motB = "0";
+      motJ = "1";
+      Serial.println("BALRA ELORE");
+      sprintf(data, "%s,%d,%s,%s,%d,%d,%d,%d,%s", bgn, potV, motB, motJ, s_Servo, h_Servo, g_Servo, x_Servo, nd);
+      //char,int,char,char,int,int,int,int,char
+      Serial.println(data);
+      Serial3.println(data);
     }
-    if (btB == 0)
+    if (btB == 0) //Ha a jobbldali fekete gomb le van nyomva, menjen jobbra
     {
       int potV = analogRead(POT);
-      potV = map(potV,POTMETER_MIN,POTMETER_MAX,0,6);
+      potV = map(potV, POTMETER_MIN, POTMETER_MAX, 0, 6);
+      motB = "1";
+      motJ = "0";
       Serial.println("JOBBRA ELORE");
+      sprintf(data, "%s,%d,%s,%s,%d,%d,%d,%d,%s", bgn, potV, motB, motJ, s_Servo, h_Servo, g_Servo, x_Servo, nd);
+      //char,int,char,char,int,int,int,int,char
+      Serial.println(data);
+      Serial3.println(data);
     }
-    if (btA == 1 && btB == 1)
+    if (btA == 1 && btB == 1) //Ha a baloldali piros és jobbldali fekete gombok nincsenek lenyomva
     {
       int potV = analogRead(POT);
-      potV = map(potV,POTMETER_MIN,POTMETER_MAX,0,6);
-      if(potV > 0)
+      potV = map(potV, POTMETER_MIN, POTMETER_MAX, 0, 6);
+      if (potV > 0) //Ha a sebesség nagyobb mint 0, menjen előre
       {
-      Serial.println("ELORE");
+        motB = "1";
+        motJ = "1";
+        Serial.println("ELORE");
+        sprintf(data, "%s,%d,%s,%s,%d,%d,%d,%d,%s", bgn, potV, motB, motJ, s_Servo, h_Servo, g_Servo, x_Servo, nd);
+        //char,int,char,char,int,int,int,int,char
+        Serial.println(data);
+        Serial3.println(data);
       }
-      else
+      else //Különben álljon meg
       {
-        Serial.println("STOP"); 
+        motB = "0";
+        motJ = "0";
+        Serial.println("STOP");
+        sprintf(data, "%s,%d,%s,%s,%d,%d,%d,%d,%s", bgn, potV, motB, motJ, s_Servo, h_Servo, g_Servo, x_Servo, nd);
+        //char,int,char,char,int,int,int,int,char
+        Serial.println(data);
+        Serial3.println(data);
       }
-    }        
+    }
   }
 }

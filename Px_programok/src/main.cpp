0aa1b50a36bf02b8e1b5cbd95d9c629131984aa8
Ármin A. Rába <47749254+ArminARaba@users.noreply.github.com>
2020-09-18 15:33:39 +0200

    init

---
+++ Px_programok/src/main.cpp
@@ -0,0 +1,35 @@
+//Változók: motorok statikusak, a pot változó, a szervók globális változók
+void P1()//Előre menet
+{
+  Serial.println("Elore");
+  while (scanner() != 0)
+  {
+    int btA = digitalRead(BA);
+    int btB = digitalRead(BB);
+    if (btA == 0)
+    {
+      int potV = analogRead(POT);
+      potV = map(potV,POTMETER_MIN,POTMETER_MAX,0,6);//kecske
+      Serial.println("BALRA ELORE"); 
+    }
+    if (btB == 0)
+    {
+      int potV = analogRead(POT);
+      potV = map(potV,POTMETER_MIN,POTMETER_MAX,0,6);
+      Serial.println("JOBBRA ELORE");
+    }
+    if (btA == 1 && btB == 1)
+    {
+      int potV = analogRead(POT);
+      potV = map(potV,POTMETER_MIN,POTMETER_MAX,0,6);
+      if(potV > 0)
+      {
+      Serial.println("ELORE");
+      }
+      else
+      {
+        Serial.println("STOP"); 
+      }
+    }        
+  }
+}
